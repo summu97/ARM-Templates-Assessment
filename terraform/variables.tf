95d2c9c4c68257fa255e0ab100144fdc9ae4f155
summu97 <135579543+summu97@users.noreply.github.com>
2024-12-26 16:55:25 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,105 +1,65 @@
 variable "location" {
-  description = "The Azure region where the resources will be created."
+  type        = string
   default     = "centralus"
+  description = "The Azure region where the resources will be created."
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group."
-  default     = "adq-az-rg"  # Default value if not provided
+  type        = string
+  default     = "adq-appdeploy-rg"
+  description = "The name of the resource group where the resources will be created."
 }
 
 variable "vnet_name" {
-  description = "The name of the virtual network."
+  type        = string
   default     = "adq-az-vnet"
+  description = "The name of the virtual network."
 }
 
 variable "vnet_cidr" {
-  description = "The CIDR block for the virtual network."
+  type        = string
   default     = "10.0.0.0/16"
+  description = "The CIDR block for the virtual network."
 }
 
 variable "bastion_subnet_name" {
-  description = "The name of the Bastion subnet."
+  type        = string
   default     = "AzureBastionSubnet"
+  description = "The name of the Bastion subnet."
 }
 
 variable "bastion_subnet_cidr" {
-  description = "The CIDR block for the Bastion subnet."
+  type        = string
   default     = "10.0.1.0/26"
+  description = "The CIDR block for the Bastion subnet."
 }
 
 variable "vm_subnet_name" {
-  description = "The name of the VM subnet."
+  type        = string
   default     = "VM-Subnet"
+  description = "The name of the VM subnet."
 }
 
 variable "vm_subnet_cidr" {
-  description = "The CIDR block for the VM subnet."
+  type        = string
   default     = "10.0.2.0/24"
+  description = "The CIDR block for the VM subnet."
 }
 
 variable "appgateway_subnet_name" {
-  description = "The name of the Application Gateway subnet."
+  type        = string
   default     = "AppGateway-Subnet"
+  description = "The name of the Application Gateway subnet."
 }
 
 variable "appgateway_subnet_cidr" {
-  description = "The CIDR block for the Application Gateway subnet."
+  type        = string
   default     = "10.0.3.0/24"
+  description = "The CIDR block for the Application Gateway subnet."
 }
 
-variable "bastion_name" {
-  description = "The name of the Bastion host."
-  default     = "adq-az-bastion"
-}
-
-variable "bastion_sku" {
-  description = "The SKU for the Bastion host."
-  default     = "Standard_B1MS"
-}
-
-variable "bastion_scale_units" {
-  description = "The number of Bastion host scale units."
-  default     = 1
-}
-
-variable "enable_ip_connect" {
-  description = "Enable IP Connect for Bastion."
-  default     = true
-}
-
-variable "enable_tunneling" {
-  description = "Enable tunneling for Bastion."
-  default     = true
-}
-
-variable "enable_shareable_link" {
-  description = "Enable shareable link for Bastion."
-  default     = false
-}
-
-variable "enable_kerberos" {
-  description = "Enable Kerberos authentication."
-  default     = false
-}
-
-variable "disable_copy_paste" {
-  description = "Disable copy-paste functionality in Bastion."
-  default     = true
-}
-
-variable "enable_session_recording" {
-  description = "Enable session recording in Bastion."
-  default     = false
-}
-
-variable "enable_private_only_bastion" {
-  description = "Enable private only Bastion."
-  default     = false
-}
-
-variable "zones" {
-  description = "The availability zones for the resources."
-  type        = list(string)
-  default     = ["1", "2"]
+variable "appgateway_name" {
+  type        = string
+  default     = "adq-az-appgateway"
+  description = "The name of the application gateway."
 }
