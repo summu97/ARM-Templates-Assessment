1d11ffe3b8d5b00602710b818f7a929953cb2fcc
summu97 <135579543+summu97@users.noreply.github.com>
2024-12-27 11:08:03 +0530

    Delete terraform directory

--- terraform/modules/resource_group/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_resource_group" "main" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
-output "name" {
-  value = azurerm_resource_group.main.name
-}
--- terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "vnet_id" {
-  description = "The ID of the created virtual network."
-  value       = module.vnet.vnet_id
-}
